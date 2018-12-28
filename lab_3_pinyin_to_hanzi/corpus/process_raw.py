6f6a9c7e1aa8d31e6ef9172077451f7337a99921
nickyc975 <chenjinlong2016@gmail.com>
2018-12-28 13:44:31 +0800

    lab_3: Update corpus, remove duplicated sentences.

--- lab_3_pinyin_to_hanzi/corpus/process_raw.py
+++ lab_3_pinyin_to_hanzi/corpus/process_raw.py
@@ -12,7 +12,7 @@ SENTENCES_FILE = "./sentences.txt"
 
 def extract_sentences(line=str):
     sentences = []
-    line = line.strip().replace(" ", "")
+    line = line.strip("\n").replace(" ", "")
 
     sentence = []
     for char in line:
@@ -26,18 +26,22 @@ def extract_sentences(line=str):
 
 def process_raw():
     files = []
+    processed_lines = []
     for root, dirnames, filenames in os.walk(RAW_DIR):
         for filename in filenames:
             if filename.endswith(".txt"):
                 files.append(os.path.join(RAW_DIR, filename))
 
-    with open(SENTENCES_FILE, "w", encoding="utf8") as sentences:
-        for raw in files:
-            with open(raw, "r", encoding="utf8") as corpus:
-                lines = corpus.readlines()
-                for line in lines:
-                    sentences.write("\n".join(extract_sentences(line)) + "\n")
+    for raw in files:
+        with open(raw, "r", encoding="utf8") as corpus:
+            lines = corpus.readlines()
+            for line in lines:
+                processed_lines.extend(extract_sentences(line))
 
+    processed_lines = set(processed_lines)
+    with open(SENTENCES_FILE, "w", encoding="utf8") as sentences:
+        for line in processed_lines:
+            sentences.write(line + "\n")
 
 if __name__ == "__main__":
     process_raw()
